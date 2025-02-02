e7fc9c8602b5ccf8be2a08d8669c5da99ce7bc37
mkmcgrath <matthew.k.mcgrath3@gmail.com>
2025-02-01 22:27:56 -0700

    complete coolpics

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,1 +1,41 @@
-//placeholder file for next weeks assignment
+const menuButton = document.querySelector("#menuBtn");
+const navLinks = document.querySelector("nav");
+
+function toggleMenu() {
+  navLinks.classList.toggle("hide");
+}
+
+menuButton.addEventListener("click", toggleMenu);
+
+// Make sure menu visibility adjusts on window resize
+function handleResize() {
+  if (window.innerWidth > 1000) {
+    navLinks.classList.remove("hide");
+  }
+}
+
+window.addEventListener("resize", handleResize);
+handleResize();
+
+function viewerTemplate(src, alt) {
+  return `
+    <div class="viewer">
+      <button class="close-viewer">X</button>
+      <img src="${src}" alt="${alt}">
+    </div>`;
+}
+
+function viewHandler(event) {
+  if (event.target.tagName === "IMG") {
+    const imgSrc = event.target.src.replace("-sm", "-full");
+    const imgAlt = event.target.alt;
+    document.body.insertAdjacentHTML("afterbegin", viewerTemplate(imgSrc, imgAlt));
+    document.querySelector(".close-viewer").addEventListener("click", closeViewer);
+  }
+}
+
+function closeViewer() {
+  document.querySelector(".viewer").remove();
+}
+
+document.querySelector(".gallery").addEventListener("click", viewHandler);
